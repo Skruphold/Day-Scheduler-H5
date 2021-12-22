d93696ab951d4e31e022bbe233b2302d95d75520
Skruphold <hunterco999@gmail.com>
2021-12-22 16:38:33 -0600

    added some var arrays for hourofday and userinput

---
+++ script.js
@@ -0,0 +1,23 @@
+var hourOfday = [
+    document.getElementById('time-9am'),
+    document.getElementById('time-10am'),
+    document.getElementById('time-11am'),
+    document.getElementById('time-12pm'),
+    document.getElementById('time-1pm'),
+    document.getElementById('time-2pm'),
+    document.getElementById('time-3pm'),
+    document.getElementById('time-4pm'),
+    document.getElementById('time-5pm'),
+];
+
+var userNotes = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
